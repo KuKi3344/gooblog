d0acd2008b99417d8a51c4c670798531f0e103c0
王璐 <568818216@qq.com>
2022-01-18 13:44:19 +0800

    引入所需插件

--- src/main.js
+++ src/main.js
@@ -2,8 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.config.productionTip = false
+
+Vue.use(ElementUI);
+import {formatTime} from './utils/time'
+
+Vue.directive('title',function(el,binding){
+	document.title = el.dataset.title
+})
+
+Vue.filter('format',formatTime)
 
 new Vue({
   router,
