412850444c7e568731b7e8afeed8536d32409b62
王璐 <568818216@qq.com>
2022-01-20 17:46:28 +0800

    封装热门文章和最新文章请求

--- src/api/article.js
+++ src/api/article.js
@@ -9,3 +9,15 @@ export function getarticles(params) {
 		data:params
 	})
 }
+export function hotArticles(params){
+	return axios({
+		methods:'get',
+		url:`${base}/api/article/hot/${params}`,
+	})
+}
+export function newArticles(){
+	return axios({
+		methods:'get',
+		url:`${base}/api/article/new`,
+	})
+}
