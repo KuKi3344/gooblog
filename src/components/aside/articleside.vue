466ee03e89da6d804f8ea2e1096d612e7e8c259d
王璐 <568818216@qq.com>
2022-01-20 17:47:31 +0800

    文章相关侧边栏

---
+++ src/components/aside/articleside.vue
@@ -0,0 +1,66 @@
+<template>
+	<el-card :body-style="{ padding: '8px 18px' }">
+	  <div slot="header" class="me-category-header">
+	    <span>{{cardHeader}}</span>
+	  </div>
+	
+	  <ul class="me-category-list">
+	    <li v-for="a in articles" @click="view(a.id)" :key="a.id" class="me-category-item"><a>{{a.title}}</a>
+	    </li>
+	  </ul>
+	</el-card>
+</template>
+
+<script>
+	export default{
+		name:'articleside',
+		props: {
+		  cardHeader: {
+		    type: String,
+		    required: true
+		  },
+		  articles: {
+		    type: Array,
+		    required: true
+		  },
+		},
+		data(){
+			return{
+				
+			}
+		},
+		methods:{
+			view(id){
+				this.$router.push({path:`/view/${id}`});
+			}
+		},
+	}
+</script>
+
+<style scoped="scoped">
+	.el-card{
+		margin-top:40px !important;
+		margin:10px;
+	}
+	.me-category-header {
+	  text-align: center;
+	  letter-spacing: 5px;
+	  font-size: 14px;
+	  
+	}
+	.me-category-list {
+	  list-style-type: none;
+	}
+	
+	.me-category-item {
+	  padding: 4px;
+	  font-size: 14px;
+	  color: #5FB878;
+	  margin-bottom: 8px;
+	  
+	}
+	
+	.me-category-item a:hover {
+	  text-decoration: underline;
+	}
+</style>
