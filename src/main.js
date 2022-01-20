c5764932d387920de9cb68d53ebd7fd093b14d61
王璐 <568818216@qq.com>
2022-01-20 17:46:11 +0800

    配置请求拦截器

--- src/main.js
+++ src/main.js
@@ -6,14 +6,24 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-
+ 
+Vue.prototype.$axios = axios;
 Vue.use(VueAxios,axios)
 Vue.use(ElementUI);
 import {formatTime} from './utils/time'
 
 Vue.config.productionTip = false;
 
-
+//请求拦截器
+axios.interceptors.request.use(config=>{
+	//如果存在token，请求携带这个token
+	if(window.sessionStorage.getItem('Authorization')){
+		config.headers['Authorization'] = window.sessionStorage.getItem('Authorization');
+	}
+	return config;
+},error=>{
+	console.log(error);
+})
 
 Vue.filter('format',formatTime)
 
